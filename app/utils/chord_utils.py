c89601339139235c008c923d4b3229d0a225dead
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-04-25 17:55:44 +0100

    chord utils is_between update

--- app/utils/chord_utils.py
+++ app/utils/chord_utils.py
@@ -5,3 +5,9 @@ def generate_id(address: tuple[str, int]):
     '''Generates an id based on SHA-1'''
     sha1_hash = hashlib.sha1(str(address).encode("utf-8")).hexdigest()
     return int(sha1_hash, 16)
+
+def is_between(start, end, node):
+    '''Check if node is between begin node and end node'''
+    if start < end:
+        return start < node <= end
+    return node > start or node <= end
