fd56d8e8dfb218d845f62b88df1063c54f9b251f
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-04-25 19:22:19 +0100

    new chord routes added

--- app/api/system/routes.py
+++ app/api/system/routes.py
@@ -1,7 +1,7 @@
 '''system blueprint'''
-import json
 from flask.views import MethodView
 from flask import Blueprint, request, current_app
+from app.utils.chord_utils import is_between
 
 blp = Blueprint("System", __name__, url_prefix="/chord")
 
@@ -9,16 +9,31 @@ blp = Blueprint("System", __name__, url_prefix="/chord")
 class ChordSuccessor(MethodView):
     '''Successor operations on chord dht protocol'''
 
-    def get(self):
-        '''Get successor'''
-        return {"message": "successor id here"}, 200
+    def post(self):
+        '''Find successor'''
+        data = request.json
+        lookup_id = data["id"]
+        requester = tuple(data["requester"].values())
+        response = current_app.node.find_successor(lookup_id, requester)
+        return response, 200
 
 
-class ChordPredecessor(MethodView):
+class ChordNotify(MethodView):
     '''Predecessor operations on chord dht protocol'''
 
-    def get(self):
-        '''Get predecessor'''
+    def post(self):
+        '''Update predecessor pointers endpoint'''
+        data = request.json
+        predecessor_id = data["predecessor_id"]
+        predecessor_addr = tuple(data["predecessor_addr"].values())
+
+        node = current_app.node
+        if node.predecessor_id is None or is_between(
+            node.predecessor_id, node.id, predecessor_id
+        ):
+            node.predecessor_id = predecessor_id
+            node.predecessor_addr = predecessor_addr
+        return {"message": "ACK"}
 
 
 class ChordJoin(MethodView):
@@ -30,7 +45,23 @@ class ChordJoin(MethodView):
         joining_id = data["id"]
         joining_addr = tuple(data["address"].values())
         response = current_app.node.handle_join_request(joining_id, joining_addr)
-        return json.dumps(response), 200
+        return response, 200
+
+
+class ChordInfo(MethodView):
+    '''Info operations on chord dht protocol'''
+
+    def get(self):
+        '''Get info from a node'''
+        node = current_app.node
+        return {
+            "id": node.id,
+            "address": node.address,
+            "successor_id": node.successor_id,
+            "successor_addr": node.successor_addr,
+            "predecessor_id": node.predecessor_id,
+            "predecessor_addr": node.predecessor_addr
+        }
 
 
 class ChordKeepAlive(MethodView):
@@ -40,7 +71,9 @@ class ChordKeepAlive(MethodView):
         '''Keep-alive heartbeat'''
         return {"message": "keep-alive, ok?"}
 
+
 blp.add_url_rule("/successor", view_func=ChordSuccessor.as_view("chord_successor"))
-blp.add_url_rule("/keepalive", view_func=ChordKeepAlive.as_view("chord_keepalive"))
-blp.add_url_rule("/predecessor", view_func=ChordPredecessor.as_view("chord_predecessor"))
+blp.add_url_rule("/notify", view_func=ChordNotify.as_view("chord_notify"))
 blp.add_url_rule("/join", view_func=ChordJoin.as_view("chord_join"))
+blp.add_url_rule("/info", view_func=ChordInfo.as_view("chord_info"))
+blp.add_url_rule("/keepalive", view_func=ChordKeepAlive.as_view("chord_keepalive"))
