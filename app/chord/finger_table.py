09b2318673b8b0e354d412a8834853c1b5ca545e
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-04-26 17:54:30 +0100

    finger table updates

--- app/chord/finger_table.py
+++ app/chord/finger_table.py
@@ -1,4 +1,5 @@
 '''finger table'''
+from math import log2
 from app.utils.chord_utils import is_between
 
 class FingerTable:
@@ -10,26 +11,23 @@ class FingerTable:
         self.m_bits = m_bits
         self.table = []
 
-        for _ in range(0, self.m_bits):
+        for _ in range(self.m_bits):
             self.table.append((node_id, node_address))
 
 
     def fill(self, node_id, node_address):
         '''Fill all entries of finger table with node_id, node_address.'''
-
-        for i in range(0, self.m_bits):
+        for i in range(self.m_bits):
             self.table[i] = (node_id, node_address)
 
 
     def update(self, index, node_id, node_address):
         '''Update index of table with node_id, node_address.'''
-
         self.table[index-1] = (node_id, node_address)
 
 
     def find(self, identification):
         '''Get node address of closest preceding node (in finger table) of id.'''
-
         for i in range(self.m_bits - 1, -1, -1):
             (node_id, node_address) = self.table[i]
             if is_between(self.node_id, identification - 1, node_id):
@@ -39,16 +37,18 @@ class FingerTable:
 
     def refresh(self):
         '''Retrieve finger table entries requiring refresh.'''
+        entries = []
+        for i in range(1, self.m_bits + 1):
+            lookup_id = (self.node_id + (2**(i-1))) % (2**self.m_bits)
+            address = self.table[self.get_index_from_id(lookup_id) - 1][1]
+            entries.append((i, lookup_id, address))
+        return entries
+
 
     def get_index_from_id(self, identification):
         '''Get index of finger table entry of id'''
+        return int(log2((identification - self.node_id) % (2**self.m_bits))) + 1
+
 
     def __repr__(self):
         return str(self.table)
-
-
-    def as_list(self):
-        '''Return the finger table as a list of tuples: (identifier, (host, port)).
-        NOTE: list index 0 corresponds to finger_table index 1
-        '''
-        return self.table
