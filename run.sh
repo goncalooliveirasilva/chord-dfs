c19d9c2e59ba30cbfb7e269e5c9c4b148517c46e
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-04-28 11:56:40 +0100

    run script

--- run.sh
+++ run.sh
@@ -3,10 +3,12 @@
 # You must have docker installed
 
 # build the Docker image
+echo "[chord-dfs] Building image"
 docker build -t chord-dfs-image .
 
 # start Docker compose services
+echo -e "\n\n[chord-dfs] Starting Docker compose services\n\n"
 docker compose up --build
 
-# To stop: press Ctrl+C
+# To stop: press Ctrl+C twice
 # Then to clean up, run: 'docker compose down'
