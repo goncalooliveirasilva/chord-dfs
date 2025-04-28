70cedc0058885b42c822b68961089e2aac80fdd9
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-04-28 11:05:25 +0100

    run script update

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# You must have docker installed
+
+# build the Docker image
 docker build -t chord-dfs-image .
+
+# start Docker compose services
 docker compose up --build
-# To stop run: docker compose down
+
+# To stop: press Ctrl+C
+# Then to clean up, run: 'docker compose down'
